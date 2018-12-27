fc869c16f739fd4a0598d3daf67da9dcbaf3c268
Todd Erickson <todd1981@gmail.com>
2018-12-26 22:47:35 -0600

    rudimentary first pass...need to break up into a cleaner structure

---
+++ alexa.go
@@ -0,0 +1,502 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	alexa "github.com/mikeflynn/go-alexa/skillserver"
+)
+
+var applications = map[string]interface{}{
+	"/echo/mbr": alexa.EchoApplication{
+		AppID:    os.Getenv("MBR_APP_ID"),
+		OnIntent: MBIntentHandler,
+		OnLaunch: MBIntentHandler,
+	},
+	"/echo/fr": alexa.EchoApplication{
+		AppID:    os.Getenv("FR_APP_ID"),
+		OnIntent: FRIntentHandler,
+		OnLaunch: FRIntentHandler,
+	},
+}
+
+var frTVActionHost = "http://192.168.72.20:8080/tv/actions"
+var mbTVActionHost = "http://192.168.72.25:8080/tv/actions"
+var frRokuActionHost = "http://192.168.72.110:8080/systems/family-room/actions"
+var mbRokuActionHost = "http://192.168.72.110:8080/systems/master-bedroom/actions"
+var frReceiverActionHost = "http://192.168.72.110:8081/receiver/actions"
+
+func main() {
+	alexa.Run(applications, "8000")
+}
+
+// FRIntentHandler is an HTTP Shandler that will accept the incoming requests to the skill and output the text response to alexa.
+func FRIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	var intent = strings.ToUpper(echoReq.GetIntentName())
+	fmt.Println("Intent passed: " + intent)
+	var output = "Processing Request."
+	switch intent {
+	case "OFF":
+		go executeAction(frTVActionHost, "PowerOff", "")
+		go updateReceiver(frReceiverActionHost, "{\"on\": false}")
+	case "MUTE":
+		go updateReceiver(frReceiverActionHost, "{\"mute\": true}")
+	case "VOLUME":
+		var slotLevel, err = echoReq.GetSlotValue("Level")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			go updateReceiver(frReceiverActionHost, "{\"volume\": -"+slotLevel+"}")
+		}
+	case "CHANNEL":
+		var slotNumber, err = echoReq.GetSlotValue("Number")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			go executeAction(frTVActionHost, "Channel", slotNumber)
+		}
+	case "CHANNELUP":
+		go executeAction(frTVActionHost, "ChannelUp", "")
+	case "CHANNELDOWN":
+		go executeAction(frTVActionHost, "ChannelDown", "")
+	case "INPUT":
+		var slotInputType, err = echoReq.GetSlotValue("InputType")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			var inputType = strings.Replace(strings.ToUpper(slotInputType), " ", "", -1)
+			frSetInput(inputType)
+		}
+	case "HOME":
+		go executeAction(frRokuActionHost, "home", "")
+	case "BACK":
+		go executeAction(frRokuActionHost, "back", "")
+	case "UP":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(frRokuActionHost, "up", spaces)
+	case "DOWN":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(frRokuActionHost, "down", spaces)
+	case "LEFT":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(frRokuActionHost, "left", spaces)
+	case "RIGHT":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(frRokuActionHost, "right", spaces)
+	case "ENTER":
+		go executeAction(frRokuActionHost, "enter", "")
+	case "SELECT":
+		go executeAction(frRokuActionHost, "select", "")
+	case "PLAY":
+		go executeAction(frRokuActionHost, "right", "")
+	case "FORWARD":
+		go executeAction(frRokuActionHost, "forward", "")
+	case "REVERSE":
+		go executeAction(frRokuActionHost, "reverse", "")
+	case "SEARCH":
+		var slotSearchType, err = echoReq.GetSlotValue("SearchType")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			go executeAction(frRokuActionHost, "search", slotSearchType)
+		}
+	default:
+		output = "I'm sorry I could not process your request " + intent + "."
+	}
+	echoResp.OutputSpeech(output)
+}
+
+// MBIntentHandler is an HTTP Shandler that will accept the incoming requests to the skill and output the text response to alexa.
+func MBIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+	var intent = strings.ToUpper(echoReq.GetIntentName())
+	fmt.Println("Intent passed: " + intent)
+	var output = "Processing Request."
+	switch intent {
+	case "OFF":
+		go executeAction(mbTVActionHost, "PowerOff", "")
+	case "MUTE":
+		go executeAction(mbTVActionHost, "Mute", "")
+	case "VOLUME":
+		var slotLevel, err = echoReq.GetSlotValue("Level")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			go executeAction(mbTVActionHost, "Volume", slotLevel)
+		}
+	case "CHANNEL":
+		var slotNumber, err = echoReq.GetSlotValue("Number")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			go executeAction(mbTVActionHost, "Channel", slotNumber)
+		}
+	case "CHANNELUP":
+		go executeAction(mbTVActionHost, "ChannelUp", "")
+	case "CHANNELDOWN":
+		go executeAction(mbTVActionHost, "ChannelDown", "")
+	case "INPUT":
+		var slotInputType, err = echoReq.GetSlotValue("InputType")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			var inputType = strings.Replace(strings.ToUpper(slotInputType), " ", "", -1)
+			mbSetInput(inputType)
+		}
+	case "HOME":
+		go executeAction(mbRokuActionHost, "home", "")
+	case "BACK":
+		go executeAction(mbRokuActionHost, "back", "")
+	case "UP":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(mbRokuActionHost, "up", spaces)
+	case "DOWN":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(mbRokuActionHost, "down", spaces)
+	case "LEFT":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(mbRokuActionHost, "left", spaces)
+	case "RIGHT":
+		var spaces = "1"
+		var slotSpaces, err = echoReq.GetSlotValue("Spaces")
+		if err == nil {
+			spaces = slotSpaces
+		}
+		go executeAction(mbRokuActionHost, "right", spaces)
+	case "ENTER":
+		go executeAction(mbRokuActionHost, "enter", "")
+	case "SELECT":
+		go executeAction(mbRokuActionHost, "select", "")
+	case "PLAY":
+		go executeAction(mbRokuActionHost, "right", "")
+	case "FORWARD":
+		go executeAction(mbRokuActionHost, "forward", "")
+	case "REVERSE":
+		go executeAction(mbRokuActionHost, "reverse", "")
+	case "SEARCH":
+		var slotSearchType, err = echoReq.GetSlotValue("SearchType")
+		if err != nil {
+			log.Fatal(err)
+			output = "I'm sorry I could not process your request " + intent + "."
+		} else {
+			go executeAction(mbRokuActionHost, "search", slotSearchType)
+		}
+	default:
+		output = "I'm sorry I could not process your request " + intent + "."
+	}
+	echoResp.OutputSpeech(output)
+}
+
+func frSetInput(inputType string) {
+	log.Println("Input passed: ", inputType)
+	var receiverPayload = "{\"on\":true, \"volume\": -30"
+	switch inputType {
+	case "TV", "T", "V":
+		receiverPayload += ", \"input\": \"AV1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "InputTV", "")
+	case "RETRO PI", "RETRO PIE", "RETROPI", "RETROPIE", "RETROPOT", "RETROBY", "RETRO", "PIE", "PI":
+		receiverPayload += ", \"input\": \"AV1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI2", "")
+	case "PSTHREE", "PS3", "THREE", "3", "P.S.3":
+		receiverPayload += ", \"input\": \"HDMI4\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "PSFOUR", "PS4", "FOUR", "4", "P.S.4":
+		receiverPayload += ", \"input\": \"HDMI2\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "WIIU", "WIYOU", "WILLYOU", "WEYOU", "WEEYOU", "WE", "WEE", "WE'LL":
+		receiverPayload += ", \"input\": \"HDMI3\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "FIRETV", "FIRE", "ROKU":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "XBOX":
+		receiverPayload += ", \"input\": \"HDMI5\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "SWITCH":
+		receiverPayload += ", \"input\": \"V-AUX\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "NETFLIX", "NET", "FLIX":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "Netflix")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "PLEX", "PLAQUES":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "Plex")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "PRIME", "AMAZON":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "Amazon Video")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "HBO":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "HBO GO")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "CRUNCHYROLL":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "Crunchyroll")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "HGTV":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "Watch HGTV")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "STARS":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "STARZ")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "PBS":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "PBS Video")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "SHOWTIME":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "Showtime Anytime")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "YOUTUBE":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "YouTube")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "NATGEOTV", "NATGEO":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "NatGeoTV")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	case "SMITHSONIAN":
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", "Smithsonian Channel")
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	default:
+		receiverPayload += ", \"input\": \"HDMI1\"}"
+		go updateReceiver(frReceiverActionHost, receiverPayload)
+		go executeAction(frRokuActionHost, "input", inputType)
+		go executeAction(frTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(frTVActionHost, "HDMI1", "")
+	}
+}
+
+func mbSetInput(inputType string) {
+	log.Println("Input passed: ", inputType)
+	switch inputType {
+	case "TV", "T", "V":
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "InputTV", "")
+	case "PS2", "TWO", "2", "PSTWO", "PS":
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "InputAV1", "")
+	case "WII", "WI", "WILL", "WE", "WEEK", "WIFI", "WEE", "WE'LL":
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "InputComponent1", "")
+	case "NETFLIX", "NET", "FLIX":
+		go executeAction(mbRokuActionHost, "input", "Netflix")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "PLEX", "PLAQUES":
+		go executeAction(mbRokuActionHost, "input", "Plex")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "PRIME", "AMAZON":
+		go executeAction(mbRokuActionHost, "input", "Amazon Video")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "HBO":
+		go executeAction(mbRokuActionHost, "input", "HBO GO")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "CRUNCHYROLL":
+		go executeAction(mbRokuActionHost, "input", "Crunchyroll")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "HGTV":
+		go executeAction(mbRokuActionHost, "input", "Watch HGTV")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "STARS":
+		go executeAction(mbRokuActionHost, "input", "STARZ")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "PBS":
+		go executeAction(mbRokuActionHost, "input", "PBS Video")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "SHOWTIME":
+		go executeAction(mbRokuActionHost, "input", "Showtime Anytime")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "YOUTUBE":
+		go executeAction(mbRokuActionHost, "input", "YouTube")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "NATGEOTV", "NATGEO":
+		go executeAction(mbRokuActionHost, "input", "NatGeoTV")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	case "SMITHSONIAN":
+		go executeAction(mbRokuActionHost, "input", "Smithsonian Channel")
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	default:
+		go executeAction(mbRokuActionHost, "input", inputType)
+		go executeAction(mbTVActionHost, "PowerOn", "")
+		time.Sleep(500 * time.Millisecond)
+		go executeAction(mbTVActionHost, "HDMI1", "")
+	}
+}
+
+func executeAction(host string, command string, action string) {
+	log.Println("host:", host)
+	var bodyStr = "{\"command\": " + command + "}"
+	if len(action) > 0 {
+		bodyStr = "{\"command\": " + command + ", \"action\": " + action + "}"
+	}
+	var jsonStr = []byte(bodyStr)
+	req, err := http.NewRequest("POST", host, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	client.Timeout = time.Second * 15
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+}
+
+// body string should look like this: {"on": true, "volume": "string", "input": "string", "mute": true } but should only include the properties that need to be updated
+func updateReceiver(host string, bodyStr string) {
+	log.Println("host:", host)
+	var jsonStr = []byte(bodyStr)
+	req, err := http.NewRequest("PUT", host, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	client.Timeout = time.Second * 15
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	log.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+}
